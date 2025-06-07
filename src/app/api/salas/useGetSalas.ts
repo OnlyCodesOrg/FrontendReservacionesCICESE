5560fdfabd4283d4cb7b649b57e1991795ce8382
Andan <141715477+AdanCC21@users.noreply.github.com>
2025-06-07 14:29:22 -0700

    pantalla de comparaciones

--- src/app/api/salas/useGetSalas.ts
+++ src/app/api/salas/useGetSalas.ts
@@ -1,12 +1,12 @@
 import { BackendRoute } from "@/types/api/route"
-export async function useGetSalasDisponibles(diaDelEvento: string, horaInicio: string, horaFin: string, salasSeleccionadas: number[]) {
+export async function useGetSalasOcupadasDisponibles(diaDelEvento: string, salasSeleccionadas: number[]) {
     try {
         const fetchData = await fetch(`${BackendRoute}/salas/listar`, {
             method: "POST",
             headers: {
                 "Content-type": "application/json"
             },
-            body: JSON.stringify({ fecha: diaDelEvento, horaInicio: horaInicio, horaFin: horaFin, salasSeleccionadas: salasSeleccionadas })
+            body: JSON.stringify({ fecha: diaDelEvento, salasSeleccionadas: salasSeleccionadas })
         });
         if (!fetchData.ok) throw new Error("Problemas con el fetch. " + fetchData.status);
 
